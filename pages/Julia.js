952b081d6ddded9ec4a011046d934d0a0b41bf7a
Julianeves01 <julianvs2007@gmail.com>
2025-04-15 11:47:34 -0300

    finished

--- pages/Julia.js
+++ pages/Julia.js
@@ -1,86 +1,119 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList, SafeAreaView, ScrollView } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 
-export default function Julia() {
-    const data = [
-        { id: '1', name: 'Julinha' },
-        { id: '2', name: 'Simões' },
-        { id: '3', name: 'Neves' },
-        { id: '4', name: 'Teste' },
-        { id: '5', name: 'Julinha' },
-        { id: '6', name: 'Simões' },
-        { id: '7', name: 'Neves' },
-        { id: '8', name: 'Teste' },
-    ]
-
-    const renderItem = ({ item }) => (
-        <View style={styles.bloco}>
-            <Text style={styles.name}>{item.name}</Text>
-        </View>
-    );
+export default function Julinha() {
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.listaContainer}>
-                <ScrollView style={styles.scrollList}>
-                    {data.map((entry) => (
-                        <View key={entry.id} style={styles.bloco}>
-                            <Text style={styles.text}>{entry.name}</Text>
-                        </View>
-                    ))}
-                </ScrollView>
-
-                <FlatList data={data} renderItem={renderItem} keyExtractor={(entry) => entry.id} style={styles.FlatList} />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.bottom}></View>
+                <View style={styles.right}>
+                <View style={styles.bottom}></View>
+                <View style={styles.bottom}></View>
+                </View>
             </View>
-        </SafeAreaView>
+        <View style={styles.main}>
+            <View style={styles.blocks}>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+            </View>
+        <ScrollView>
+            <View style={styles.cards}>
+                <View style={styles.card}>
+                    <image style={styles.imageCard} source={require("../assets/teste.jpg")} />
+                    <Text style={styles.text}>Ola amores</Text>
+                </View>
+            <View style={styles.card}>
+                <image style={styles.imageCard} source={require ("../assets/teste.jpg")} />
+                <Text style={styles.text}>teste teste 2</Text>
+            </View>
+            <View style={styles.card}>
+                <image style={styles.imageCard} source={require("../assets/teste.jpg")} />
+                <Text style={styles.text}>teste teste 3</Text>
+            </View>
+            </View>
+        </ScrollView>
+        </View>
+        </View>
+
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        display: 'flex',
+        flexDirection: "column",
         backgroundColor: '#fff',
         alignItems: 'Center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
-    listaContainer: {
-        flex: 1,
+    header: {
         width: '100%',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
+        height: 100,
+        position: 'fixed',
+        top: 65,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        position: 'fixed',
+        gap: '8rem',
     },
-    scrollList: {
-        marginBottom: 20,
-        paddingVertical: 10,
+    right: {
+        display: 'flex',
+        flexDirection: 'row',
     },
-    bloco: {
-        backgroundColor: 'pink',
-        padding: 14,
-        marginVertical: 8,
-        borderRadius: 8,
-        alignItems: "center",
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 3,
+    bottom: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#000',
+        borderRadius: 100,
     },
-    name: {
-        fontSize: 19,
-        fontWeight: '600',
-        color: "#333",
-    },
-    FlatList: {
+    main: {
         width: '100%',
-        marginTop: 10,
+        height: '82.2%',
+        position: 'fixed',
+        bottom: '0',
+    },
+    blocks: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginBottom: '20px'
+    },
+    box: {
+        width: 170,
+        height: 170,
+        borderWidth: 2,
+        borderRadius: 30,
+    },
+    cards: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 30,
     },
     text: {
+        width: 350,
+        padding: 3,
         fontSize: 16,
-        color: "#333",
     },
-
+    card: {
+        width: 360,
+        height: 250,
+        borderRadius: 10,
+        borderWidth: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageCard: {
+        width: 358,
+        height: 180,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
 })
