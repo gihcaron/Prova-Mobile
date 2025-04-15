88d1f8643eff4788d772b397ab87c6225df38abf
Flavia Mendes <flavinharamendes@gmail.com>
2025-04-15 11:46:23 -0300

    fix: textos

--- pages/Flavia.js
+++ pages/Flavia.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import {StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View} from 'react-native';
 
-export default function Flavia () {
+import { Image } from 'react-native-web';
+export default function Flavia() {
+    return (
     <View style={styles.container}>
-        <Image style={styles.icone} source={require('./../assets/seta-esquerda.png')}/>
-        <Image style={styles.imagem} source={require('./../assets/hellocafe.jpg')}/>
-        <Image style={styles.imagem} source={require('./../assets/hellocomputador.jpg')}/>
-        <Image style={styles.imagem} source={require('./../assets/hellolivro.jpg')}/>
-        <Text style={styles.description}>Atividade avaliativa de mobile para testar nossos conhecimentos gerais e desenvolver melhor nossas habilidades técnicas.</Text>
-        <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Perfil</Text>
-        </TouchableOpacity>
+        <Image source ={require('./../assets/seta-esquerda.png')}style={styles.Icon}></Image>
+        <Image source ={require('./../assets/hellocafe.jpg')}style={styles.Image}></Image>
+        <Image source ={require('./../assets/hellocomputador.jpg')}style={styles.Image}></Image>
+        <Image source ={require('./../assets/hellolivro.jpg')}style={styles.Image}></Image>
+    <Text styles>Atividade avaliativa de mobile para testar nossos conhecimentos gerais e desenvolver melhor nossas habilidades técnicas. Nessa atividade nós criamos um aplicativo funcional, utilizando os conceitos de navegação, componentes visuais e organização com React Expo Native.
+    </Text>
+    <TouchableOpacity>
+        
+    </TouchableOpacity>
     </View>
+);
 }
 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-   },
- });
+    const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+    //lembrete*******: estilizar imagens, colocar uma do lado da outra com rolagem
+    //icon no canto da tela parte superior
+});
+
+//NÃO ESTÁ ROLANDO PRA BAIXO e não aparece o texto (diminuir imagem)!!!!!!!! ARRUMAR***
